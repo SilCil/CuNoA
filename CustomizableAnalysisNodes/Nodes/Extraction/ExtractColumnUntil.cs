79d9792817c83d0432bb509cdf0aecb25a11ac95
SilCil <65537555+SilCil@users.noreply.github.com>
2022-10-05 16:58:11 +0900

    Create ExtractColumnUntil.cs

---
+++ CustomizableAnalysisNodes/Nodes/Extraction/ExtractColumnUntil.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace CustomizableAnalysisLibrary.Nodes
+{
+    [Node("抽出/列/指定した列番号まで取り出し")]
+    public class ExtractColumnUntil : IOptionNode, ICalculationNode
+    {
+        public int Index { get; set; } = 0;
+
+        public IEnumerable<(string label, Value)> GetOptions()
+        {
+            yield return ("列番号", new Value(Index));
+        }
+
+        public void SetOptions(params Value[] options)
+        {
+            Index = options[0].ToIntValue().IntValue;
+        }
+
+        public Table Run(Table data)
+        {
+            var columns = new List<IReadOnlyList<Value>>();
+            
+            for(int i = 0; i < data.ColumnCount; ++i)
+            {
+                if (i <= Index)
+                {
+                    columns.Add(data.GetColumn(i));
+                }
+            }
+
+            return Table.CreateFromColumns(columns);
+        }
+    }
+}
